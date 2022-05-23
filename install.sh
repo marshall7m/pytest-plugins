c5922e2ff1f003b8cc02356cb591f34d75392e2d
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-05-23 16:40:54 -0700

    use python:3.9-slim-buster and remove actions/setup-python@v3 for testing to skip slow lbzip2 install needed for actions/setup-python@v3 (this problem has been detected within local act env --  could be not an issue with remote gh action runner)

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 
 #!/bin/bash
-apt-get update
 apt-get -y update
-apt-get install -y aptitude
-aptitude install -y software-properties-common build-essential wget unzip git
+apt-get -y install --no-install-recommends wget unzip curl git
 
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+python3 -m pip install -e .
 
 wget -q -O /tmp/tfenv.tar.gz https://github.com/tfutils/tfenv/archive/refs/tags/v${TFENV_VERSION}.tar.gz
 tar -zxf /tmp/tfenv.tar.gz -C /tmp
 mkdir /usr/local/.tfenv && mv /tmp/tfenv-${TFENV_VERSION}/* /usr/local/.tfenv && chmod u+x /usr/local/.tfenv/bin/tfenv
+echo "/usr/local/.tfenv/bin" >> $GITHUB_PATH
 
 chmod u+x /usr/local/bin/*
-apk del .build-deps
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 rm -rf /tmp/*
-rm -rf /var/cache/apk/*
