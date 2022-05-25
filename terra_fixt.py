9a6b2baaccf47d96b730a878dc5c1254f2118449
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-05-24 18:00:40 -0700

    rm tf plan/apply/output fixt and add tf_factory fixt

--- terra_fixt.py
+++ terra_fixt.py
@@ -19,16 +19,6 @@ def pytest_addoption(parser):
         help="skips initing testing Terraform module",
     )
     group.addoption(
-        "--skip-tf-plan",
-        action="store_true",
-        help="skips planning testing Terraform module",
-    )
-    group.addoption(
-        "--skip-tf-apply",
-        action="store_true",
-        help="skips applying testing Terraform module",
-    )
-    group.addoption(
         "--skip-tf-destroy",
         action="store_true",
         help="skips destroying testing Terraform module",
@@ -73,29 +63,12 @@ def terra_version(binary: str, version: str, overwrite=False):
             text=True,
         )
     except subprocess.CalledProcessError as e:
-        log.debug(e.stderr)
+        log.error(e, exc_info=True)
         raise e
 
 
-def tf_vars_to_json(tf_vars: dict) -> dict:
-    for k, v in tf_vars.items():
-        if type(v) not in [str, bool, int, float]:
-            tf_vars[k] = json.dumps(v)
-
-    return tf_vars
-
-
-@pytest.fixture(scope="session")
-def terraform_version(request):
-    """Terraform version that will be installed and used"""
-    terra_version("terraform", request.param, overwrite=True)
-    return request.param
-
-
-@pytest.fixture(scope="session")
-def tf(request, terraform_version: str):
-    log.info(f"Terraform Version: {terraform_version}")
-    tf = tftest.TerraformTest(request.param)
+def _init(request, *args, **kwargs):
+    tf = tftest.TerraformTest(*args, **kwargs)
 
     if request.config.getoption("skip_tf_init"):
         log.info("--skip-tf-init is set -- skipping Terraform init")
@@ -107,49 +80,8 @@ def tf(request, terraform_version: str):
 
         log.info("Running Terraform init")
         tf.setup(upgrade=True, cleanup_on_exit=cleanup_on_exit)
-
-    yield tf
-
-    log.info("Running Terraform init")
-    tf.setup(upgrade=True, cleanup_on_exit=cleanup_on_exit)
-    tf_destroy(request.config.getoption("skip_tf_destroy"), tf)
-
-
-@pytest.fixture(scope="session")
-def tf_plan(request, tf):
-    if request.config.getoption("skip_tf_plan"):
-        pytest.skip(
-            "--skip-tf-plan is set -- "  # noqa: E501
-            "skipping tests depending on terraform plan",  # noqa: E501
-        )
-
-    if hasattr(request, "param"):
-        tf_param = tftest.TerraformTest(request.param)
-        yield tf_param.plan(output=True)
-        # need return to separate parametrized fixture and factory fixture
-        return
-
-    response = []
-
-    def _plan(update=True, **tf_vars):
-        """
-        Returns the Terraform plan output.
-        Arguments:
-            update: If True, runs the Terraform command and returns the output.
-                    If False, returns the cached Terraform plan from the
-                    previous function call.
-        """
-        if update:
-            response.clear()
-            response.append(
-                tf.plan(
-                    output=True,
-                    tf_vars=tf_vars_to_json(tf_vars),
-                )
-            )
-        return response[0]
-
-    yield _plan
+        
+    return tf
 
 
 def tf_destroy(skip, tf):
@@ -164,61 +96,35 @@ def tf_destroy(skip, tf):
 
 
 @pytest.fixture(scope="session")
-def tf_apply(request, tf):
-    if request.config.getoption("skip_tf_apply"):
-        pytest.skip(
-            "--skip-tf-apply is set -- "  # noqa: E501
-            "skipping tests depending on terraform apply"  # noqa: E501
-        )
-
-    if hasattr(request, "param"):
-        tf_param = tftest.TerraformTest(request.param)
-        yield tf_param.apply(auto_approve=True)
-        # need return to separate parametrized fixture and factory fixture
-        return
-
-    response = []
-
-    def _apply(update=True, **tf_vars):
-        """
-        Returns the Terraform apply output.
-        Arguments:
-            update: If True, runs the Terraform command and returns the output.
-                    If False, returns the cached Terraform apply output from
-                    the previous function call.
-        """
-
-        if update:
-            response.clear()
-            response.append(
-                tf.apply(auto_approve=True, tf_vars=tf_vars_to_json(tf_vars))
-            )
-        return response[0]
-
-    yield _apply
+def terraform_version(request):
+    """Terraform version that will be installed and used"""
+    terra_version("terraform", request.param, overwrite=True)
+    return request.param
 
 
 @pytest.fixture(scope="session")
-def tf_output(request, tf):
-    if hasattr(request, "param"):
-        tf_param = tftest.TerraformTest(request.param)
-        yield tf_param.output()
-        # need return to separate parametrized fixture and factory fixture
-        return
+def tf_factory(request):
+    tf_cfgs = []
+    def _tf(*args, **kwargs):
+        tf = _init(request, *args, **kwargs)
+        tf_cfgs.append(tf)
+        return tf
+    yield _tf
 
-    response = []
+    for tf in tf_cfgs:
+        tf_destroy(request.config.getoption("skip_tf_destroy"), tf)
 
-    def _output(update=True):
-        """
-        Returns the Terraform plan output.
-        Arguments:
-            update: If True, runs the Terraform command and returns the output.
-                    If False, returns the cached Terraform output from the
-                    previous function call.
-        """
-        if update:
-            response.clear()
-            response.append(tf.output())
-        return response[0]
 
-    yield _output
+@pytest.fixture(scope="session")
+def tf(request, terraform_version: str):
+    log.info(f"Terraform Version: {terraform_version}")
+    if type(request.param) == list:
+        tf = _init(request, *request.param)
+    elif type(request.param) == dict:
+        tf = _init(request, **request.param)
+    else:
+        tf = _init(request, request.param)
+    
+    yield tf
+    
+    tf_destroy(request.config.getoption("skip_tf_destroy"), tf)
