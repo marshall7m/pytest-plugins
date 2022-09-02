abc267a43fc2d2cf248be57c3999508dfbfa21b2
Marshall Mamiya <44485531+marshall7m@users.noreply.github.com>
2022-09-02 15:16:43 -0700

    hook changes

--- tests/test_terra_cache.py
+++ tests/test_terra_cache.py
@@ -1,21 +1,24 @@
-import pytest
-
 pytest_plugins = [
     str("_pytest.pytester"),
 ]
 
+
 def test_initial_cache(pytester):
-    pytester.makepyfile("""
-    
+    pytester.makepyfile(
+        """
+
     def test_foo(terra_cache):
         assert terra_cache() == {}
-    """)
+    """
+    )
     reprec = pytester.inline_run()
 
     reprec.assertoutcome(passed=1)
 
+
 def test_add_cache(pytester):
-    pytester.makepyfile("""
+    pytester.makepyfile(
+        """
     from terra_fixt import TfTestCache
     def test_foo(terra_cache):
         data = {
@@ -25,13 +28,16 @@ def test_add_cache(pytester):
         }
         terra_cache(**data)
         assert isinstance(terra_cache()["foo/bar"], TfTestCache)
-    """)
+    """
+    )
     reprec = pytester.inline_run()
 
     reprec.assertoutcome(passed=1)
 
+
 def test_update_cache(pytester):
-    pytester.makepyfile("""
+    pytester.makepyfile(
+        """
     from tftest import TerragruntTest
     def test_foo(terra_cache):
         data = {
@@ -40,12 +46,13 @@ def test_update_cache(pytester):
             "basedir": "foo"
         }
         terra_cache(**data)
-        
+
         data["binary"] = "terragrunt"
         terra_cache(**data)
 
         assert isinstance(terra_cache()["foo/bar"].instance, TerragruntTest)
-    """)
+    """
+    )
     reprec = pytester.inline_run()
 
     reprec.assertoutcome(passed=1)
--- tests/test_tftest_cache.py
+++ tests/test_tftest_cache.py
@@ -2,9 +2,10 @@ import pytest
 from unittest.mock import patch
 from terra_fixt import TfTestCache
 
+
 @pytest.mark.parametrize("cmd", ["plan", "apply", "destroy", "init", "state_pull"])
 def test_get_cache(cmd):
-    
+
     cache = TfTestCache(binary="terraform", tfdir="foo")
     with patch(f"tftest.TerraformTest.{cmd}") as mock_cmd:
         output = cache.run_terra_cmd(cmd)
